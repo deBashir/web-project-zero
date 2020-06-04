5a8a584ec0eb4b3f962a85289362a7183dd234f9
Bashir <aaleks19981@gmail.com>
2020-06-04 15:19:13 +0300

    change

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  db: 'mongodb+srv://Bash:12345@cluster0-fx6is.mongodb.net/test?retryWrites=true&w=majority',
+  db: 'mongodb+srv://Bash:a0102b@cluster0-fx6is.mongodb.net/test?retryWrites=true&w=majority',
   secret: 'some_itproger'
 }
